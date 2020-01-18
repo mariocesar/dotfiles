e4d3ec156c030b0df73d5bfdaec55057bd24187e
Mario César <mariocesar.c50@gmail.com>
2020-01-18 14:34:36 -0400

    Update install

--- install.py
+++ install.py
@@ -6,51 +6,73 @@ import os
 from pathlib import Path
 from itertools import chain
 
-root_dir = Path(__file__)
-home_dir = Path(os.path.expanduser('~'))
+ROOT_DIR = Path(__file__).parent.resolve()
+HOME_DIR = Path.home()
 
-os.chdir(root_dir.parent.resolve())
+print(ROOT_DIR, HOME_DIR)
 
-all_files = set(chain(
-    glob.glob('.*/**', recursive=True),
-    glob.glob('.*', recursive=True),
-    glob.glob('*', recursive=True),
-))
-
-all_files = sorted(list(all_files))
+os.chdir(ROOT_DIR.parent)
 
 exclude_patterns = [
-    re.compile(r'^\.git/').match,
-    re.compile(r'^\.dotfiles/').match,
-    re.compile(r'^\.gitignore$').match,
-    re.compile(r'^.*~$').match,
-    re.compile(r'\.pyc$').match,
-    re.compile(r'README\.md').match,
-    re.compile(r'install\.py').match,
+    re.compile(r"^\.git/").match,
+    re.compile(r"^\.vscode$").match,
+    re.compile(r"^\.gitignore$").match,
+    re.compile(r"\.pyc$").match,
+    re.compile(r"~$").match,
+    re.compile(r"README\.md").match,
+    re.compile(r"install\.py").match,
 ]
 
-match_any = lambda path: any(map(lambda match: match(path), exclude_patterns))
+skip = lambda path: any(
+    map(lambda match: match(str(path.relative_to(ROOT_DIR))), exclude_patterns)
+)
 
-filtered_files = filter(lambda path: not match_any(path), all_files)
-filtered_files = filter(lambda path: not os.path.isdir(path), filtered_files)
-filtered_files = map(lambda path: Path(path).resolve(), filtered_files)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Install dotfiles')
-    parser.add_argument('--force',
-        action='store_true', default=False,
-        help='If target file exists it replaces it')
+def walk(path: Path):
+    for item in path.glob("*"):
+        if skip(item):
+            continue
+
+        if item.is_dir():
+            yield from walk(item)
+        else:
+            yield item
+
+
+dotfiles = walk(ROOT_DIR)
+
+
+def main(options):
+    for path in dotfiles:
+        path = (ROOT_DIR / (path.relative_to(ROOT_DIR))).resolve()
+        dest = (HOME_DIR / (path.relative_to(ROOT_DIR))).resolve()
+
+        print(f"{dest.relative_to(HOME_DIR)} ", end="", flush=True)
+
+        if options.force and dest.is_file():
+            dest.unlink()
+            print(f"[Force]", end=" ", flush=True)
+
+        if dest.parent != ROOT_DIR:
+            if not dest.parent.exists():
+                dest.parent.mkdir(parents=True)
+
+        if dest.exists():
+            print(f"[Ok]")
+        else:
+            dest.symlink_to(path)
+            print(f"[Installed]")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Install dotfiles")
+    parser.add_argument(
+        "--force",
+        action="store_true",
+        default=False,
+        help="If target file exists it replaces it",
+    )
 
     options = parser.parse_args()
 
-    for path in filtered_files:
-        dst_path = os.path.expanduser('~') / path
-
-        print(f'{dst_path} ', end='')
-
-        if dst_path.exists():
-            print(f'[Ok]')
-        else:
-            path.symlink_to(dst_path.resolve())
-            print(f'[Done]')
-
+    main(options)
