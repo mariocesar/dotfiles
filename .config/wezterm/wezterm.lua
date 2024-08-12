2eb6f8201271354cb515774902cfa9312e3de80c
Mario-César <mariocesar.c50@gmail.com>
2024-08-12 18:48:46 -0400

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Tokyo Night'
+config.window_decorations = 'RESIZE'
+config.initial_cols = 100
+config.initial_rows = 40
+
+
+config.leader = {
+  key = 'a',
+  mods = 'CTRL',
+  timeout_milliseconds = 1000
+}
+
+config.keys = {
+  {
+    key = '"',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '%',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+}
+
+return config
